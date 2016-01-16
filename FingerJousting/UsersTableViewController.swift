b8df67d7d34bed2d2b3a85572d12ba6a8bca1374
David Wolgemuth <davidjwolgemuth@gmail.com>
2016-01-16 15:27:49 -0800

    Alert Button and TableView

---
+++ FingerJousting/UsersTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  UsersTableViewController.swift
+//  FingerJousting
+//
+//  Created by David Wolgemuth on 1/16/16.
+//  Copyright © 2016 E-D. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewController: UITableViewController
+{
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        askForUsernameAlert()
+    }
+    func askForUsernameAlert()
+    {
+        let alert = UIAlertController(title: "UserName", message: "Please Enter a UserName", preferredStyle: .Alert)
+        let save = UIAlertAction(title: "Save", style: .Default) {
+            (action: UIAlertAction!) -> Void in
+            let userName = alert.textFields![0]
+            print(userName.text)
+        }
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        alert.addAction(save)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+}
