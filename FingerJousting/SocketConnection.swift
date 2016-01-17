bcc7f24deb448fab85987bfeb4551c3b189fe735
David Wolgemuth <davidjwolgemuth@gmail.com>
2016-01-16 19:38:03 -0800

    Sending and Receiving Game Requests

--- FingerJousting/SocketConnection.swift
+++ FingerJousting/SocketConnection.swift
@@ -11,6 +11,8 @@ import UIKit
 protocol UsersController
 {
     func usersArrayDidFill()
+    func requestReceived(fromUser user: String)
+    func gameDidStart(isPlayer1 player1: Bool)
 }
 
 class Connection
@@ -31,38 +33,48 @@ class Connection
         socket = SocketIOClient(socketURL: "Eugenes-MacBook-Pro.local:8000")
         socket?.connect()
         socket?.on("connect") { data, ack in
-            print("iPhone connected to server")
+            print("Connected to Server")
         }
-        
     }
-    
     func requestGame(withUser user: String) {
         socket?.emit("requestGame", user)
     }
-    
-    func getAllUsers(controller: UsersController) {
+    func waitForRequests(controller: UsersController)
+    {
+        socket?.on("user-request") { data, ack in
+            print("Request Received")
+            let user = data[0] as! String
+            controller.requestReceived(fromUser: user)
+        }
+        socket?.on("start-game") { data, ack in
+            let playersTurn = data[0] as! Bool
+            controller.gameDidStart(isPlayer1: playersTurn)
+        }
+    }
+    func getAllUsers(controller: UsersController)
+    {
         socket?.emit("all-users")
         socket?.on("all-users") { data, ack in
+            print("List of Users Received")
             self.fillAllUsers(data)
             controller.usersArrayDidFill()
         }
     }
-    
     private func fillAllUsers(data: [AnyObject]) {
-        print("Data: \(data)")
         self.allUsers = data[0] as! [String]
     }
-    
     func sendToServer(GameMoves move: [Int]) {
         socket?.emit("ToServer", move)
     }
-    
+    func sendResponseToInvitation(response: Bool)
+    {
+        socket?.emit("response", response)
+    }
     func sendToServer(userName name: String) {
         socket?.emit("user-name", name)
     }
     private init()
     {
         connectToServer()
-        print("Initin")
     }
 }
